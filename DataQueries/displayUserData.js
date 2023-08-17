b2b6243e26593201f731657680b1926155088bfa
itsgurpreetsingh <gurpreet.gsr50@gmail.com>
2023-08-17 12:15:35 +0530

    update

---
+++ DataQueries/displayUserData.js
@@ -0,0 +1,24 @@
+// const { connection } = require("./dbconfig.js");
+//
+// async function displayTable() {
+//     const selectUsers = "SELECT name, roll_num, phone_number, password from users;";
+//     // const initdbStatement = "USE LMS;";
+//
+//     // await connection.query(initdbStatement, (error, results, fields) => {
+//     //     // console.log(error);
+//     //     // console.log(results);
+//     //     // console.log(fields);
+//     //     // console.log('/////////////////');
+//     // });
+//
+//     await connection.query(selectUsers, (error, results, fields) => {
+//         // console.log(error);
+//         // console.log(results);
+//         // console.log(fields);
+//         // console.log('/////////////////');
+//     });
+// }
+//
+// module.exports = {
+//     displayTable
+// };
